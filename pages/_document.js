f5ee42b5212ac1a041d9f0b325332ee99aee3895
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2023-08-10 11:58:35 +0600

    Initial Setup

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,60 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                {/* Fav Icon */}
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/images/common/favicon.svg"
+                />
+
+                {/* Font Awesome */}
+                <link
+                    rel="stylesheet"
+                    href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css"
+                />
+
+                <link
+                    rel="stylesheet"
+                    href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-solid.css"
+                />
+
+                <link
+                    rel="stylesheet"
+                    href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-regular.css"
+                />
+
+                <link
+                    rel="stylesheet"
+                    href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-light.css"
+                />
+
+                {/* Google Font */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </Head>
+
+            <body>
+                <Main />
+                <NextScript />
+
+                {/* Flowbite */}
+                <script
+                    src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"
+                    async
+                ></script>
+            </body>
+        </Html>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        colors: {
+            primary: {
+                50: "#b3e6fa",
+                100: "#99def8",
+                200: "#80d6f7",
+                300: "#66cef5",
+                400: "#4dc6f3",
+                500: "#33bdf1",
+                600: "#1ab5f0",
+                700: "#00adee",
+                800: "#009cd6",
+                900: "#008abe",
+                950: "#0079a7",
+            },
+        },
+
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [require("flowbite/plugin")],
+};
