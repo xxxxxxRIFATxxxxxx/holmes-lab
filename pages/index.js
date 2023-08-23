40260a0ca47b7a57e04c8015b4b9e0c6a9c60606
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2023-08-23 16:43:04 +0600

    done

--- pages/index.js
+++ pages/index.js
@@ -494,7 +494,7 @@ export default function Home() {
                         })}
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                         {showProducts.map((product) => {
                             if (product?.category === "AR Ad") {
                                 return (
